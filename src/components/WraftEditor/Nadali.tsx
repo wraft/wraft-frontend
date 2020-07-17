4a76e8754dadc85937f76837af40997eb284ddf7
Muneef Hameed <hey@muneef.in>
2020-07-17 20:28:07 +0530

    new editor

---
+++ src/components/WraftEditor/Nadali.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Text } from 'rebass';
+
+// Menu Template - ?
+
+export const Menu = (props: any) => (
+  <Box pt={1} sx={{ p: 1, width: 'auto' }} {...props}>
+    <Text fontSize={0}>Menu</Text>
+  </Box>
+);
+
+
+// Text Editor - Remirror
+
+export const TextEditor = (props: any) => (
+  <Box pt={1} sx={{ p: 1, width: 'auto' }} {...props}>
+    <Text fontSize={0}>TextEditor</Text>
+  </Box>
+);
