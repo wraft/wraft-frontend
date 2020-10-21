99e90abe48cb17c6a64b6a21e0eca54d5659c2fd
Muneef Hameed <hey@muneef.in>
2020-10-21 18:18:06 +0530

    company member stuffs

---
+++ pages/account/company.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Head from 'next/head';
+import ProfileForm from '../../src/components/OrgForm';
+import Page from '../../src/components/Page';
+import { Flex } from 'rebass';
+import Link from 'next/link';
+
+export const CompanyForm = () => {
+  return (
+    <>
+      <Head>
+        <title>Manage Organization | Wraft Doc</title>
+        <meta name="description" content="Wraft Docs" />
+      </Head>
+      <Page>
+      <Flex>
+          <Link href="/account">
+            <a>Back to company</a>
+          </Link>
+        </Flex>
+        <Flex>         
+        <Link href="/account/members">
+            <a>Members</a>
+          </Link>          
+          <ProfileForm/>
+        </Flex>
+      </Page>
+    </>
+  );
+};
+
+export default CompanyForm;
---
+++ src/components/OrgForm.tsx
@@ -0,0 +1,280 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { useStoreState } from 'easy-peasy';
+
+import { Button, Alert, Close, Spinner, Box, Text, Flex } from 'theme-ui';
+// import { Label, Select, Textarea } from '@rebass/forms';
+
+import Field from './Field';
+import { checkUser, createEntity, loadEntity } from '../utils/models';
+
+export interface Profile {
+  allergies?: string[];
+  profile: ProfileClass;
+  risks?: string[];
+}
+
+export interface ProfileClass {
+  activity_level_id: string;
+  calory_range_id: string;
+  dob: string;
+  gender: string;
+  goal_id: string;
+  height: number;
+  menu_id: string;
+  mobile_number: string;
+  profile_id: string;
+  weight: number;
+}
+
+const formList = [
+  {
+    id: 'name',
+    label: 'Company Name',
+    ftype: 'text',
+    defaultValue: 'Functionary Films',
+  },
+  {
+    id: 'legal_name',
+    label: 'Legal Name',
+    ftype: 'text',
+    defaultValue: 'Functionary Films Pvt Ltd',
+  },
+  {
+    id: 'addres',
+    label: 'Address',
+    ftype: 'text',
+    defaultValue: '#21, HM Green Oak Apartments, Bengaluru, IN',
+  },
+  {
+    id: 'corporate_id',
+    label: 'Corporate ID',
+    ftype: 'text',
+    defaultValue: 'AHEPH-XXXX',
+  },
+  {
+    id: 'name_of_ceo',
+    label: 'CEO Full Name',
+    ftype: 'text',
+    defaultValue: 'Muneef Hameed',
+  },
+  {
+    id: 'name_of_cto',
+    label: 'CTO Full Name',
+    ftype: 'text',
+    defaultValue: 'Salsabeel Jamal',
+  },
+  {
+    id: 'phone',
+    label: 'Phone Number',
+    ftype: 'text',
+    defaultValue: '8050473500',
+  },
+  {
+    id: 'gstin',
+    label: 'GST Number',
+    ftype: 'text',
+    defaultValue: 'GST-IN-XXX',
+  },
+  {
+    id: 'email',
+    label: 'Email Address',
+    ftype: 'text',
+    defaultValue: 'info@aurut.com',
+  },
+];
+
+const OrgForm = () => {
+  const { register, handleSubmit, errors, setValue } = useForm();
+  const token = useStoreState(state => state.auth.token);
+  const [ready, setReady] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [profile, setProfile] = useState<any>();
+  const [organ, setOrgan] = useState<any>();
+
+  const onCreate = (d: any) => {
+    setSuccess(true);
+    console.log('__d', d);
+    // if (d && d.id) {
+    //   // Router.push(`/user-profile`);
+    // }
+  };
+
+  /** Send Invite Form */
+
+  const onInviteSubmit = (data: any) => {
+    console.log('data', data);
+    createEntity(data, `organisations/${data?.id}`, token, onCreate);
+  }
+
+  /** Update Form */
+
+  
+  const onSubmit = (data: any) => {
+    console.log('data', data);
+    createEntity(data, 'organisations', token, onCreate);
+    // updateEntity('organisations', data, token, onCreate);
+  };
+
+  useEffect(() => {
+    console.log('errors', errors);
+  }, [errors]);
+
+  const onProfileLoad = (data: any) => {
+    const res: any[] = data;
+    setProfile(res);
+  };
+
+  useEffect(() => {
+    // check if token is there
+    // const tokenInline = cookie.get('token') || false;
+    //
+    if (token) {
+      checkUser(token, onProfileLoad);
+      // loadEntity(token, 'macros', loadDataSuccess);
+    }
+  }, [token]);
+
+  /**
+   * When Org data is load
+   *
+   */
+  const onOrgLoad = (_o: any) => {
+    setReady(true);
+    console.log('profile.organisation_id', _o);
+    setOrgan(_o);
+
+    Object.keys(_o).map(function(key, index) {
+      console.log('key', key, index, `${key}`, _o[`${key}`]);
+      setValue(`${key}`, _o[`${key}`]);
+    });
+
+    // set sample
+  };
+
+  const onOrgMembLoad = (_o: any) => {
+    setReady(true);
+    console.log('onOrgMembLoad', _o);
+    // setOrgan(_o);
+
+    // Object.keys(_o).map(function(key, index) {
+    //   console.log('key', key, index, `${key}`, _o[`${key}`]);
+    //   setValue(`${key}`, _o[`${key}`]);
+    // });
+  };
+
+  /**
+   * Set Profile Context
+   */
+
+  useEffect(() => {
+    // check if token is there
+    // const tokenInline = cookie.get('token') || false;
+    //
+    if (profile) {
+      console.log('profile', profile);
+      // setValue('')
+      // checkUser(token, onProfileLoad);
+      loadEntity(token, `organisations/${profile.organisation_id}`, onOrgLoad);
+      loadEntity(
+        token,
+        `organisations/${profile.organisation_id}/memberships`,
+        onOrgMembLoad,
+      );
+    }
+
+    // if(organ) {
+    //   console.log('got organ', organ);
+    // }
+  }, [profile]);
+
+  /**
+   * Watch Form Change
+   */
+  const checkChange = (_a: any) => {
+    console.log('__args', _a);
+  };
+
+  return (
+    <Box py={3} px={6} variant="w70" mt={4}>
+      {!ready && <Spinner />}
+      {success && (
+        <Alert>
+          Category created succesfully!
+          <Close ml="auto" mr={-2} />
+        </Alert>
+      )}
+
+      <Box>
+        <Text variant="pagetitle">Create Organization</Text>
+        {profile?.name}
+
+        {ready && (
+          <Box
+            mx={0}
+            mb={3}
+            variant="w100"
+            as="form"
+            onSubmit={handleSubmit(onSubmit)}>
+            {formList.map((fl: any) => (
+              <Box>
+                <Field
+                  name={fl.id}
+                  label={fl.label}
+                  defaultValue={fl.defaultValue}
+                  register={register}
+                />
+              </Box>
+            ))}
+
+            {/* <Field
+              name="dob"
+              label="Birthday"
+              defaultValue="1992-09-24"
+              register={register}
+            />
+            <Label>
+              <Select name="gender" ref={register({ required: true })}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </Select>
+            </Label> */}
+            <Button type="submit" ml={2} mt={3}>
+              Update Profile
+            </Button>
+          </Box>
+        )}
+      </Box>
+
+      <Box>
+        <Box>
+          <Text variant="pagetitle">Invite Members</Text>
+          <Box
+            mx={0}
+            mb={3}
+            variant="w100"
+            as="form"
+            onSubmit={handleSubmit(onInviteSubmit)}>
+            <Field
+              name="organisation_id"
+              label="Org ID"
+              defaultValue={organ?.id}
+              register={register}
+            />
+            <Field
+              name="email"
+              label="Email Address"
+              defaultValue="anan@aurut.com"
+              register={register}
+            />
+            <Button type="submit" ml={2} mt={3}>
+              Update Profile
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+export default OrgForm;
