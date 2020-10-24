3b1b63b421612b14625a9d92b2bb51332ffa5969
Muneef Hameed <hey@muneef.in>
2020-10-24 12:01:14 +0530

    api path from ENV

--- src/utils/models.tsx
+++ src/utils/models.tsx
@@ -7,7 +7,7 @@
 // const mx = 'http://localhost:4000' 'https://dieture.x.aurut.com'
 // const API_HOST = process.env.API_HOST || 'http://localhost:4000' // 'https://api.o.dieture.com';
 // export const API_HOST = process.env.API_HOST || 'http://localhost:4000' //'https://wraft.x.aurut.com'
-export const API_HOST = process.env.API_HOST || ' https://wraft.x.aurut.com' // 'https://api.o.dieture.com';
+export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || '' // 'https://api.o.dieture.com';
 import cookie from 'js-cookie';
 /**
  * Load Entity
