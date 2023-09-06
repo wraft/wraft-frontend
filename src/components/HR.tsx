3fcaaa78f1e3547cf6a9811ef542e8ee3f733174
haroonalbar <haroonalbarx@gmail.com>
2023-09-06 15:38:57 +0530

    feat: updated HR to take in color

--- src/components/HR.tsx
+++ src/components/HR.tsx
@@ -2,31 +2,22 @@ import { Box } from 'theme-ui';
 
 interface props {
   my?: string;
+  color?: string;
 }
 
-const HR = ({ my }: props) => {
-  if (my)
-    return (
-      <Box
-        sx={{
-          width: '100%',
-          height: '1px',
-          my: `${my}`,
-          bg: 'neutral.0',
-        }}
-      />
-    );
-  else
-    return (
-      <Box
-        sx={{
-          width: '100%',
-          height: '1px',
-          my: '200px',
-          bg: 'neutral.0',
-        }}
-      />
-    );
+const HR = ({ my, color }: props) => {
+  const margin = my ? `${my}` : '200px';
+  const colorSelected = color ? `${color}` : 'neutral.0';
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '1px',
+        my: margin,
+        bg: colorSelected,
+      }}
+    />
+  );
 };
 
 export default HR;
