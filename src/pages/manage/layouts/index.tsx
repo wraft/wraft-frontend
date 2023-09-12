c0dc7c84a7df9cc6f879fb14e2c3002563828647
Haroon Albar <haroon.albar@functionary.co>
2023-09-12 12:42:25 +0530

    feat: use modal instead of path for rendering LayoutForm

--- src/pages/manage/layouts/index.tsx
+++ src/pages/manage/layouts/index.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import Head from 'next/head';
-import { Flex, Container } from 'theme-ui';
+import { Flex, Container, Button, Close } from 'theme-ui';
 import LayoutList from '../../../components/LayoutList';
+import LayoutForm from '../../../components/LayoutForm';
 import Page from '../../../components/PageFrame';
 import ManageSidebar from '../../../components/ManageSidebar';
 import { HeadingFrame } from '../../../components/Card';
 import Link from '../../../components/NavLink';
 import { menuLinks } from '../../../utils';
+import Modal from 'react-modal';
 
 const Index: FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <>
       <Head>
@@ -19,11 +22,31 @@ const Index: FC = () => {
         <HeadingFrame
           title="Manage"
           side={
-            <Link variant="btnPrimary" href="/manage/layouts/new">
-              Add Layout
-            </Link>
+            // <Link variant="btnPrimary" href="/manage/layouts/new">
+            //   Add Layout
+            // </Link>
+            <Button onClick={() => setIsOpen(true)}>Add Laybout</Button>
           }
         />
+        <Modal
+          isOpen={isOpen}
+          style={{
+            overlay: {
+              backgroundColor: 'rgba(0, 0, 0, 0.50)',
+            },
+            content: {
+              marginLeft: 'auto',
+              minWidth: '582px',
+              width: '40%',
+              height: '100%',
+              right: '0px',
+              top: '0px',
+              borderRadius: '0px',
+            },
+          }}>
+          <Close onClick={() => setIsOpen(false)} sx={{ cursor: 'pointer' }} />
+          <LayoutForm />
+        </Modal>
         <Container sx={{ px: 4, pt: 0 }}>
           <Flex>
             <ManageSidebar items={menuLinks} />
