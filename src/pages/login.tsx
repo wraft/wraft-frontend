6b551d166734154324fb5e4af27363b484ec32ce
Harshad <mohamad.harshad@functionary.co>
2023-09-15 10:47:34 +0530

    loginpage integration

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import Head from 'next/head';
-import { Box } from 'theme-ui';
 // import Container from '../src/components/Container';
-import UserNav from '../components/UserNav';
-import UserLoginForm from '../components/UserLoginForm';
+import UserLogin from '../components/userLogin';
 
 export const Index: FC = () => {
   return (
@@ -15,10 +13,8 @@ export const Index: FC = () => {
           content="Wraft - The Document Automation Platform"
         />
       </Head>
-      <Box>
-        <UserNav />
-        <UserLoginForm />
-      </Box>
+
+      <UserLogin />
     </>
   );
 };
