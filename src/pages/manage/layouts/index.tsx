cd2e9c92c420035654f5f0ff503172722e0efe42
Salsabeel <k.salsabeel@gmail.com>
2023-09-26 22:41:43 +0530

    fix: css fix

--- src/pages/manage/layouts/index.tsx
+++ src/pages/manage/layouts/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import Head from 'next/head';
-import { Flex, Container, Button } from 'theme-ui';
+import { Flex, Container, Button, Box } from 'theme-ui';
 
 import LayoutList from '../../../components/LayoutList';
 import LayoutForm from '../../../components/LayoutForm';
@@ -25,12 +25,15 @@ const Index: FC = () => {
         <ModalLeft isOpen={isOpen} setOpen={setIsOpen}>
           <LayoutForm />
         </ModalLeft>
-        <Container sx={{ px: 4, pt: 0 }}>
-          <Flex>
-            <ManageSidebar items={menuLinks} />
-            <LayoutList />
-          </Flex>
-        </Container>
+
+        <Box variant="layout.pageFrame" pt={0}>
+          <Container>
+            <Flex>
+              <ManageSidebar items={menuLinks} />
+              <LayoutList />
+            </Flex>
+          </Container>
+        </Box>
       </Page>
     </>
   );
