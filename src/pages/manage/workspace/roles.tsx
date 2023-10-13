62b12bfb85c4af24d9d25abfc868477cf263a905
haroonalbar <haroonalbarx@gmail.com>
2023-10-13 11:05:20 +0530

    feat: roles progress

--- src/pages/manage/workspace/roles.tsx
+++ src/pages/manage/workspace/roles.tsx
@@ -8,10 +8,17 @@ import ManageSidebar from '../../../components/ManageSidebar';
 import { workspaceLinks } from '../../../utils';
 import ModalCustom from '../../../components/ModalCustom';
 import { InviteUserIcon } from '../../../components/Icons';
-// import Table from '../../../components/Table';
 
 const Index: FC = () => {
+  const token = useStoreState((state) => state.auth.token);
   const [isOpen, setIsOpen] = React.useState(false);
+  function onsuccess(data: any) {
+    console.log(data);
+  }
+  loadEntity(token, 'roles', onsuccess);
+  React.useEffect(() => {
+    loadEntity;
+  }, []);
   return (
     <>
       <Head>
@@ -19,67 +26,29 @@ const Index: FC = () => {
         <meta name="description" content="a nextjs starter boilerplate" />
       </Head>
       <Page>
-        <PageHeader title="Manage Layouts" desc="Document Layouts">
+        <PageHeader title="Manage roles" desc="Document Layouts">
           <Button
+            // variant="btnPrimary"
             onClick={() => setIsOpen(true)}
             sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-            <InviteUserIcon />
-            Invite people
+            <AddIcon />
+            Create new role
           </Button>
         </PageHeader>
-        <ModalCustom varient="right" isOpen={isOpen} setOpen={setIsOpen}>
-          <div></div>
+        <ModalCustom isOpen={isOpen} setOpen={setIsOpen}>
           {/* <LayoutForm /> */}
         </ModalCustom>
-        <Container sx={{ px: 4, pt: 0 }}>
+        <Container
+          sx={{
+            px: 4,
+            pt: 0,
+            height: '100%',
+            maxHeight: '90vh',
+            overflow: 'auto',
+            bg: 'background',
+          }}>
           <Flex>
             <ManageSidebar items={workspaceLinks} />
-            <Flex>
-              <Box
-                sx={{
-                  bg: 'bgWhite',
-                  border: '1px solid',
-                  borderColor: 'neutral.1',
-                  borderRadius: 4,
-                  flexGrow: 1,
-                  // p: 4,
-                  m: 4,
-                  mr: 0,
-                }}>
-                {/* <Table
-                  options={{
-                    columns: [
-                      {
-                        Header: '',
-                        accessor: 'col0', // accessor is the "key" in the data
-                        width: 'auto',
-                      },
-                      {
-                        Header: 'Name',
-                        accessor: 'col1',
-                        width: '59%',
-                      },
-                      {
-                        Header: 'Time',
-                        accessor: 'col2',
-                        width: '14%',
-                      },
-                      {
-                        Header: 'Editors',
-                        accessor: 'col3',
-                        width: '14%',
-                      },
-                      {
-                        Header: 'Status',
-                        accessor: 'status',
-                        width: '14%',
-                      },
-                    ],
-                    data: vendors,
-                  }}
-                /> */}
-              </Box>
-            </Flex>
             {/* <Workspace /> */}
           </Flex>
         </Container>
