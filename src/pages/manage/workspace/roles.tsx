d4e7c0e1c6b85bb136784f0092cce488ca2899e4
haroonalbar <haroonalbarx@gmail.com>
2023-10-18 11:29:33 +0530

    chore: pause roles

--- src/pages/manage/workspace/roles.tsx
+++ src/pages/manage/workspace/roles.tsx
@@ -8,6 +8,7 @@ import ManageSidebar from '../../../components/ManageSidebar';
 import { workspaceLinks } from '../../../utils';
 import ModalCustom from '../../../components/ModalCustom';
 import { InviteUserIcon } from '../../../components/Icons';
+// import Table from '../../../components/Table';
 
 const Index: FC = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -26,12 +27,59 @@ const Index: FC = () => {
             Invite people
           </Button>
         </PageHeader>
-        <ModalCustom isOpen={isOpen} setOpen={setIsOpen}>
+        <ModalCustom varient="right" isOpen={isOpen} setOpen={setIsOpen}>
+          <div></div>
           {/* <LayoutForm /> */}
         </ModalCustom>
         <Container sx={{ px: 4, pt: 0 }}>
           <Flex>
             <ManageSidebar items={workspaceLinks} />
+            <Flex>
+              <Box
+                sx={{
+                  bg: 'bgWhite',
+                  border: '1px solid',
+                  borderColor: 'neutral.1',
+                  borderRadius: 4,
+                  flexGrow: 1,
+                  // p: 4,
+                  m: 4,
+                  mr: 0,
+                }}>
+                {/* <Table
+                  options={{
+                    columns: [
+                      {
+                        Header: '',
+                        accessor: 'col0', // accessor is the "key" in the data
+                        width: 'auto',
+                      },
+                      {
+                        Header: 'Name',
+                        accessor: 'col1',
+                        width: '59%',
+                      },
+                      {
+                        Header: 'Time',
+                        accessor: 'col2',
+                        width: '14%',
+                      },
+                      {
+                        Header: 'Editors',
+                        accessor: 'col3',
+                        width: '14%',
+                      },
+                      {
+                        Header: 'Status',
+                        accessor: 'status',
+                        width: '14%',
+                      },
+                    ],
+                    data: vendors,
+                  }}
+                /> */}
+              </Box>
+            </Flex>
             {/* <Workspace /> */}
           </Flex>
         </Container>
