55ae9739ff2d2d9625c238b77e4770a8bf4a7dd9
haroonalbar <haroonalbarx@gmail.com>
2023-10-19 19:51:52 +0530

    feat: added OptionsIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -92,6 +92,20 @@ export const Abstract = () => (
 
 export const UserIcon = () => <Image className="x" src={userno} width="32" />;
 
+export const OptionsIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="36"
+    height="18"
+    viewBox="0 0 36 18"
+    fill="none">
+    <rect width="36" height="18" fill="white" />
+    <circle cx="8.5" cy="8.5" r="2.5" fill="#C1C6DB" />
+    <circle cx="17.5" cy="8.5" r="2.5" fill="#C1C6DB" />
+    <circle cx="26.5" cy="8.5" r="2.5" fill="#C1C6DB" />
+  </svg>
+);
+
 export const AddIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
