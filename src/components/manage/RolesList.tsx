d4f29bc7e66c9d854d6a0bc476201013aaac16fc
Haroon <haroon.albar@functionary.co>
2023-10-24 18:04:33 +0530

    feat: refined delete option

--- src/components/manage/RolesList.tsx
+++ src/components/manage/RolesList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Text, Flex } from 'theme-ui';
+import { Box, Text, Flex, Button } from 'theme-ui';
 import { transparentize } from '@theme-ui/color';
 import { deleteEntity, loadEntity } from '../../utils/models';
 import { useStoreState } from 'easy-peasy';
 
 import { Table } from '../Table';
 import ContentLoader from 'react-content-loader';
-import { OptionsIcon } from '../Icons';
+import { ErrorIcon, OptionsIcon } from '../Icons';
 import ModalCustom from '../ModalCustom';
 
 export interface RolesItem {
@@ -37,11 +37,13 @@ const RolesList = () => {
     }
   }, [token]);
 
-  const [isOpen, setIsOpen] = useState<number | null>(0);
+  const [isOpen, setIsOpen] = useState<number | null>(null);
+  const [isDelete, setIsDelete] = useState<number | null>(null);
+  useEffect(() => setIsOpen(null), []);
   useEffect(() => {
     if (contents && contents.length > 0) {
       const row: any = [];
-      contents.map((r: any, index: number) => {
+      contents.map((r: any) => {
         const rFormated = {
           name: (
             <Text variant="text.pM" sx={{ color: 'gray.5' }}>
@@ -154,68 +156,144 @@ const RolesList = () => {
                 accessor: 'col3',
                 Cell: ({ row }) => {
                   return (
-                    <Box
-                      sx={{ cursor: 'pointer', position: 'relative' }}
-                      onClick={() => {
-                        setIsOpen(row.index);
-                        console.log(row);
-                        console.log(contents[row.index]);
-                      }}
-                      onMouseLeave={() => setIsOpen(null)}>
-                      <OptionsIcon />
-                      {isOpen === row.index ? (
-                        <Box
-                          sx={{
-                            position: 'absolute',
-                            bg: 'bgWhite',
-                            // p: 3,
-                            right: 0,
-                            top: 0,
-                            zIndex: 10,
-                            border: '1px solid',
-                            borderColor: 'neutral.1',
-                            width: '155px',
-                          }}>
+                    <>
+                      <Box
+                        sx={{ cursor: 'pointer', position: 'relative' }}
+                        onClick={() => {
+                          setIsOpen(row.index);
+                          // console.log(row);
+                          // console.log(contents[row.index]);
+                        }}
+                        onMouseLeave={() => setIsOpen(null)}
+                        // onMouseOut={() => setIsOpen(null)}
+                      >
+                        <OptionsIcon />
+                        {isOpen === row.index ? (
                           <Box
-                            onClick={() => console.log('edit', row.index)}
-                            variant="text.pM"
                             sx={{
-                              color: 'gray.6',
-                              ':hover': {
-                                bg: transparentize('neutral.1', 0.5),
-                              },
-                              p: 3,
+                              position: 'absolute',
+                              bg: 'bgWhite',
+                              // p: 3,
+                              right: 0,
+                              top: 0,
+                              zIndex: 10,
+                              border: '1px solid',
+                              borderColor: 'neutral.1',
+                              width: '155px',
                             }}>
-                            Edit
+                            <Button
+                              disabled={
+                                contents[row.index].name === 'superadmin'
+                              }
+                              onClick={() => console.log('edit', row.index)}
+                              variant="text.pM"
+                              sx={{
+                                textAlign: 'left',
+                                width: '100%',
+                                bg: 'bgWhite',
+                                color: 'gray.6',
+                                p: 3,
+                                ':hover': {
+                                  bg: transparentize('neutral.1', 0.5),
+                                },
+                                ':disabled': {
+                                  color: 'gray.2',
+                                },
+                              }}>
+                              Edit
+                            </Button>
+                            <Button
+                              disabled={
+                                contents[row.index].name === 'superadmin'
+                              }
+                              variant="text.pM"
+                              onClick={() => {
+                                setIsOpen(null);
+                                setIsDelete(row.index);
+                              }}
+                              sx={{
+                                textAlign: 'left',
+                                width: '100%',
+                                bg: 'bgWhite',
+                                color: 'red.5',
+                                p: 3,
+                                ':hover': {
+                                  bg: transparentize('neutral.1', 0.5),
+                                },
+                                ':disabled': {
+                                  color: 'gray.2',
+                                },
+                              }}>
+                              Delete
+                            </Button>
                           </Box>
-                          <Box
-                            variant="text.pM"
-                            onClick={() => {
-                              deleteEntity(
-                                `roles/${contents[row.index].id}`,
-                                token,
-                              );
-                            }}
+                        ) : (
+                          <Box />
+                        )}
+                      </Box>
+                      <ModalCustom
+                        // transparent
+                        varient="center"
+                        isOpen={isDelete === row.index}
+                        setOpen={setIsDelete}>
+                        {contents[row.index] &&
+                        contents[row.index].user_count > 0 ? (
+                          <Flex
                             sx={{
-                              color: 'red.5',
-                              ':hover': {
-                                bg: transparentize('neutral.1', 0.5),
-                              },
-                              p: 3,
+                              flexDirection: 'column',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              p: 4,
+                              gap: 3,
                             }}>
-                            Delete
+                            <ErrorIcon />
+                            <Text>You cannot remove a role that is in use</Text>
+                          </Flex>
+                        ) : (
+                          <Box>
+                            <Flex
+                              sx={{
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                p: 4,
+                                gap: 3,
+                              }}>
+                              <Box>
+                                Are you sure you want to delete{' '}
+                                <Text
+                                  as={'span'}
+                                  sx={{ display: 'inline-block' }}>
+                                  {contents[row.index]?.name}
+                                </Text>
+                                ?
+                              </Box>
+                              <Box>
+                                <Flex sx={{ gap: 3 }}>
+                                  <Button
+                                    variant="delete"
+                                    onClick={() => {
+                                      deleteEntity(
+                                        `roles/${contents[row.index].id}`,
+                                        token,
+                                      );
+                                      loadData(token);
+                                      setIsDelete(null);
+                                    }}>
+                                    Confirm
+                                  </Button>
+                                  <Button
+                                    variant="cancel"
+                                    onClick={() => setIsDelete(null)}>
+                                    Cancel
+                                  </Button>
+                                </Flex>
+                              </Box>
+                            </Flex>
                           </Box>
-                          {/* <ModalCustom
-                            varient="center"
-                            isOpen={contents[row.index].user_count > 0}
-                            setOpen={}>
-                            <Box>You cannot remove a role that is in use</Box>
-                          </ModalCustom> */}
-                        </Box>
-                      ) : (
-                        <Box />
-                      )}
-                    </Box>
+                        )}
+                      </ModalCustom>
+                    </>
                   );
                 },
                 width: '3%',
