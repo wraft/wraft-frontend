7db5cb15d75e4358295a82e1f1bf9531fe2f6c02
Haroon Albar <haroon.albar@functionary.co>
2023-11-16 02:13:24 +0530

    fix: style

--- src/components/manage/PermissionsList.tsx
+++ src/components/manage/PermissionsList.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource theme-ui */
 import React, { useEffect, useMemo, useState } from 'react';
-import { Box, Button, Flex } from 'theme-ui';
+import { Box, Button, Flex, Text } from 'theme-ui';
 import { loadEntity, updateEntity } from '../../utils/models';
 import { useStoreState } from 'easy-peasy';
 import TableNew from '../TableNew';
@@ -155,21 +155,22 @@ const PermissionsList = () => {
     render;
   }, [permissions]);
 
-  const ColumnRoles = roles.map((role: any) => {
-    const svgDataUri =
-      encodeURIComponent(`<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const svgDataUri =
+    encodeURIComponent(`<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g id="Icon/tick">
 <path id="Vector" d="M11.7709 3.33641C11.7048 3.26509 11.6261 3.20848 11.5394 3.16985C11.4527 3.13122 11.3597 3.11133 11.2657 3.11133C11.1718 3.11133 11.0788 3.13122 10.9921 3.16985C10.9054 3.20848 10.8267 3.26509 10.7605 3.33641L5.45949 9.01284L3.23235 6.62356C3.16367 6.55262 3.08259 6.49683 2.99375 6.45939C2.90491 6.42195 2.81004 6.40359 2.71457 6.40536C2.61909 6.40712 2.52488 6.42898 2.4373 6.46969C2.34972 6.51039 2.2705 6.56914 2.20416 6.64259C2.13782 6.71603 2.08565 6.80273 2.05064 6.89774C2.01563 6.99274 1.99846 7.09419 2.00011 7.19629C2.00176 7.29839 2.0222 7.39914 2.06027 7.49279C2.09833 7.58645 2.15327 7.67116 2.22195 7.74211L4.95429 10.664C5.02044 10.7353 5.09914 10.792 5.18585 10.8306C5.27256 10.8692 5.36556 10.8891 5.45949 10.8891C5.55343 10.8891 5.64643 10.8692 5.73314 10.8306C5.81985 10.792 5.89855 10.7353 5.96469 10.664L11.7709 4.45495C11.8432 4.3837 11.9008 4.29722 11.9402 4.20096C11.9796 4.10471 12 4.00076 12 3.89568C12 3.7906 11.9796 3.68665 11.9402 3.59039C11.9008 3.49414 11.8432 3.40766 11.7709 3.33641Z" fill="#343E49"/>
 </g>
 </svg>
   `);
-    const svgDataUriWhite =
-      encodeURIComponent(`<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const svgDataUriWhite =
+    encodeURIComponent(`<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g id="Icon/tick">
 <path id="Vector" d="M11.7709 3.33641C11.7048 3.26509 11.6261 3.20848 11.5394 3.16985C11.4527 3.13122 11.3597 3.11133 11.2657 3.11133C11.1718 3.11133 11.0788 3.13122 10.9921 3.16985C10.9054 3.20848 10.8267 3.26509 10.7605 3.33641L5.45949 9.01284L3.23235 6.62356C3.16367 6.55262 3.08259 6.49683 2.99375 6.45939C2.90491 6.42195 2.81004 6.40359 2.71457 6.40536C2.61909 6.40712 2.52488 6.42898 2.4373 6.46969C2.34972 6.51039 2.2705 6.56914 2.20416 6.64259C2.13782 6.71603 2.08565 6.80273 2.05064 6.89774C2.01563 6.99274 1.99846 7.09419 2.00011 7.19629C2.00176 7.29839 2.0222 7.39914 2.06027 7.49279C2.09833 7.58645 2.15327 7.67116 2.22195 7.74211L4.95429 10.664C5.02044 10.7353 5.09914 10.792 5.18585 10.8306C5.27256 10.8692 5.36556 10.8891 5.45949 10.8891C5.55343 10.8891 5.64643 10.8692 5.73314 10.8306C5.81985 10.792 5.89855 10.7353 5.96469 10.664L11.7709 4.45495C11.8432 4.3837 11.9008 4.29722 11.9402 4.20096C11.9796 4.10471 12 4.00076 12 3.89568C12 3.7906 11.9796 3.68665 11.9402 3.59039C11.9008 3.49414 11.8432 3.40766 11.7709 3.33641Z" fill="#ffffff"/>
 </g>
 </svg>
   `);
+
+  const ColumnRoles = roles.map((role: any) => {
     return {
       header: role.name,
       accessorKey: role.name,
@@ -245,10 +246,7 @@ const PermissionsList = () => {
       accessorKey: 'name',
       id: 'name',
       cell: ({ row, getValue }: any) => (
-        <Box
-          sx={{
-            paddingLeft: `${row.depth * 2}rem`,
-          }}>
+        <Box>
           {row.getCanExpand() ? (
             <Button
               variant="base"
@@ -257,7 +255,9 @@ const PermissionsList = () => {
                 style: { cursor: 'pointer', width: '100%' },
               }}>
               <Flex sx={{ gap: '8px' }}>
-                <Box>{getValue()}</Box>
+                <Text variant="pM" sx={{ color: 'gray.6' }}>
+                  {getValue()}
+                </Text>
                 <Box
                   sx={{
                     display: 'flex',
@@ -271,7 +271,17 @@ const PermissionsList = () => {
               </Flex>
             </Button>
           ) : (
-            <Box>{getValue()}</Box>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                px: 3,
+                py: 2,
+              }}>
+              <Text variant="pM" sx={{ color: 'green.6' }}>
+                {getValue()}
+              </Text>
+            </Box>
           )}
         </Box>
       ),
