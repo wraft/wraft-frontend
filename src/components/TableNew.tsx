a329035097c62d55f9e66dd1d704900982f15c29
Haroon <haroon.albar@functionary.co>
2023-11-16 02:13:24 +0530

    fix: style

--- src/components/TableNew.tsx
+++ src/components/TableNew.tsx
@@ -13,6 +13,38 @@ export interface TableProps {
   data: any;
   columns: any;
 }
+// export const styles = {
+//   table: {
+//     borderCollapse: 'collapse',
+//     width: '100%',
+//     border: '1px solid',
+//     borderColor: 'neutral.1',
+//     textAlign: 'left',
+//     bg: 'bgWhite',
+//   } as ThemeUIStyleObject,
+//   thead: {
+//     bg: 'gray.0',
+//     fontSize: 0,
+//     textTransform: 'uppercase',
+//     border: '1px solid',
+//     borderColor: 'neutral.1',
+//     color: 'gray.1',
+//   },
+//   tr: {
+//     verticalAlign: 'top',
+//     // bg: 'gray.0',
+//     borderBottom: '1px solid',
+//     borderBottomColor: 'neutral.1',
+//     '&:last-child': {
+//       border: '0',
+//     },
+//     '&:hover': {
+//       bg: 'background',
+//     },
+//   },
+//   th: { p: 2, textAlign: 'left', fontWeight: 'normal' },
+//   td: { p: 2 },
+// };
 
 export const TableNew = ({ data, columns }: TableProps) => {
   const [expanded, setExpanded] = React.useState<ExpandedState>({});
@@ -53,7 +85,7 @@ export const TableNew = ({ data, columns }: TableProps) => {
                 // maxWidth: header.column.columnDef.maxWidth,
               } as ThemeUIStyleObject;
               return (
-                <Box as="th" key={header.id} sx={thSx}>
+                <Box as="th" key={header.id} sx={{ ...thSx }}>
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext(),
@@ -67,7 +99,17 @@ export const TableNew = ({ data, columns }: TableProps) => {
       <Box as="tbody">
         {table.getRowModel().rows.map((row) => {
           return (
-            <Box as="tr" key={row.id} sx={styles.tr}>
+            <Box
+              as="tr"
+              key={row.id}
+              sx={{
+                ...styles.tr,
+                border: (row.depth > 0 || row.getIsExpanded()) && 'none',
+                backgroundColor: row.depth > 0 && 'background',
+                ':hover': {
+                  backgroundColor: row.getIsExpanded() && 'transparent',
+                },
+              }}>
               {row.getVisibleCells().map((cell) => (
                 <Box as="th" variant="text.subR" key={cell.id} sx={styles.td}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
