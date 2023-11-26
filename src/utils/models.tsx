ff728c2fdc435af8b4cfe65f3d17df92dc97d5a1
Haroon Albar <haroonalbarx@gmail.com>
2023-11-27 00:47:00 +0530

    feat: used axios on createEnity in modals

--- src/utils/models.tsx
+++ src/utils/models.tsx
@@ -136,34 +136,93 @@ export const loadEntityDetail = (
  * @param path
  * @param token
  */
-export const createEntity = (
+
+// if (!response.ok) {
+//   return response.json().then((error) => {
+//     throw new Error(error.error); // Throwing an error with the specific message
+//   });
+// }
+// return response.json();
+// if (!response.ok)
+//   const errorData = await response.json();
+//   const errorMessage = errorData?.errors?.name?.[0];
+
+//   if (errorMessage) {
+//     console.error(errorMessage);
+//   } else {
+//     throw new Error('Unexpected error format');
+//   }
+// console.log(response);
+// return response.json();
+// }
+// export const createEntity = async (
+//   data: any,
+//   path: string,
+//   token: string,
+//   onSuccess?: any,
+//   onFailed?: any,
+// ) => {
+//   console.log('🥷', API_HOST, data);
+
+//   await fetch(`${API_HOST}/api/v1/${path}`, {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${token}`,
+//     },
+//     body: JSON.stringify(data),
+//   })
+//     .then(async function (response) {
+//       console.log(response);
+//       if (!response.ok) {
+//         throw Error(`HTTP error! Status: ${response.status}`);
+//       }
+//     })
+//     .then(function (data) {
+//       if (onSuccess) {
+//         console.log(`Created a model ${path} with Pass`, data);
+//         onSuccess(data);
+//       } else {
+//         console.log(`Created a model ${path}`, data);
+//       }
+//     })
+//     .catch((error) => {
+//       onFailed(error);
+//       console.error('Errorrrrrrrrrrr:', error);
+//     });
+// };
+
+export const createEntity = async (
   data: any,
   path: string,
   token: string,
   onSuccess?: any,
+  onFailed?: any,
 ) => {
   console.log('🥷', API_HOST, data);
 
-  fetch(`${API_HOST}/api/v1/${path}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      if (onSuccess) {
-        console.log(`Created a model ${path} with Pass`, data);
-        onSuccess(data);
-      } else {
-        console.log(`Created a model ${path}`, data);
-      }
+  try {
+    const response = await axios.post(`${API_HOST}/api/v1/${path}`, data, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
     });
+
+    console.log(response);
+
+    if (onSuccess) {
+      console.log(`Created a model ${path} with Pass`, response.data);
+      onSuccess(response.data);
+    } else {
+      console.log(`Created a model ${path}`, response.data);
+    }
+  } catch (error) {
+    onFailed(error);
+    console.error('Errorrrrrrrrrrr:', error);
+  }
 };
 
 /**
