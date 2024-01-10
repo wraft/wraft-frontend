c9063757d678d13322af5d6c669692983014fa48
Haroon Albar <haroon.albar@functionary.co>
2024-01-10 14:47:19 +0530

    fix: document creation error on produce

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { AxiosRequestConfig, AxiosError } from 'axios';
-import produce from 'immer';
+import { produce } from 'immer';
 import cookie from 'js-cookie';
 
 import { Layout, User, Style, FlowBranch } from '../components/Icons';
