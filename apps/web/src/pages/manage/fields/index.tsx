a253f2243d22297f77f663e634814a35e71a28a7
Haroon Albar <haroonalbarx@gmail.com>
2024-01-21 20:02:26 +0530

    fix(web): layout issue on field , added sidebar also

--- apps/web/src/pages/manage/fields/index.tsx
+++ apps/web/src/pages/manage/fields/index.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
 
 import Head from 'next/head';
+import { Box, Container, Flex } from 'theme-ui';
 
 import FieldList from '../../../components/FieldList';
+import ManageSidebar from '../../../components/ManageSidebar';
+import Link from '../../../components/NavLink';
 import Page from '../../../components/PageFrame';
+import PageHeader from '../../../components/PageHeader';
+import { menuLinks } from '../../../utils';
 
 const Index: FC = () => {
   return (
@@ -13,7 +18,21 @@ const Index: FC = () => {
         <meta name="description" content="a nextjs starter boilerplate" />
       </Head>
       <Page>
-        <FieldList />
+        <PageHeader title="Fields Types" desc="Manage System Level fields">
+          <Box sx={{ ml: 'auto', mr: 0, mt: 2 }}>
+            <Link href="/manage/fields/new" variant="btnSecondary">
+              + New Field
+            </Link>
+          </Box>
+        </PageHeader>
+        <Container variant="layout.pageFrame">
+          <Flex>
+            <ManageSidebar items={menuLinks} />
+            <Box variant="layout.contentFrame">
+              <FieldList />
+            </Box>
+          </Flex>
+        </Container>
       </Page>
     </>
   );
