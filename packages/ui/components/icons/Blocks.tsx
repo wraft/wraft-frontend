ccb86a273be4c7be11468cc0e116ac29d7a69ae2
thalha <thalhaz999@gmail.com>
2024-01-24 12:28:42 +0530

    feat :svgr component

---
+++ packages/ui/components/icons/Blocks.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+const SvgBlocks = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    data-color="red"
+    viewBox="0 0 20 24"
+    {...props}>
+    <path d="M5 8h2V6h3.252L7.68 18H5v2h8v-2h-2.252L13.32 6H17v2h2V4H5z" />
+  </svg>
+);
+export default SvgBlocks;
---
+++ packages/ui/components/icons/index.tsx
@@ -0,0 +1,2 @@
+export { default as Approvals } from './Approvals';
+export { default as Blocks } from './Blocks';
