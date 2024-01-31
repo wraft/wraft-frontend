2f373371c60b68efb843a3c75fd583258e3c9ba1
Salsabeel <k.salsabeel@gmail.com>
2024-01-31 19:38:18 +0530

    chore(docs): icon added on storybook

--- apps/docs/stories/icons.stories.tsx
+++ apps/docs/stories/icons.stories.tsx
@@ -1,87 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import * as Icon from '@wraft/icon';
 
-const iconNames = ['Add', 'Up', 'Down'];
+interface Icons {
+  [key: string]: any;
+}
+
+const icons: Icons = Icon;
 
 export const Icons = () => {
   return (
     <>
-      <div style={{display: 'flex', alignItems: 'center', paddingBottom: '20px',gap:'5px'}}>
-        <Icon.AddIcon />
-        <span>Add</span>
-        <Icon.AddPeopleIcon />
-        <span>AddPeople</span>
-        <Icon.ArrowDownIcon />
-        <span>ArrowDown</span>
-        <Icon.ArrowLeftIcon />
-        <span>ArrowLeft</span>
-        <Icon.ArrowRightIcon />
-        <span>ArrowRight</span>
-        <Icon.ArrowUpIcon />
-        <span>ArrowUp</span>
-        <Icon.BoldIcon />
-        <span>Bold</span>
-        <Icon.BulletListIcon />
-        <span>BulletList</span>
-        <Icon.CloseIcon />
-        <span>Close</span>
-        <Icon.DeleteIcon />
-        <span>Delete</span>
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', paddingBottom:'20px', gap:'7px' }}>
-        
-        <Icon.DownIcon />
-        <span>Down</span>
-        <Icon.EditIcon />
-        <span>Edit</span>
-        <Icon.EllipsisHIcon />
-        <span>EllipsisH</span>
-        <Icon.EllipsisVIcon />
-        <span>EllipsisV</span>
-        <Icon.EyeIcon />
-        <span>Eye</span>
-        <Icon.EyeClosedIcon />
-        <span>EyeClosed</span>
-        <Icon.FilterIcon />
-        <span>Filter</span>
-        <Icon.H1Icon />
-        <span>H1</span>
-        <Icon.H2Icon />
-        <span>H2</span>
-        <Icon.H3Icon />
-        <span>H3</span>
-        <Icon.HomeIcon/>
-        <span>Home</span>
-        <Icon.UpIcon />
-        <span>Up</span>
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center',gap:'5px' }}>
-        <Icon.ItalicsIcon />
-        <span>Italics</span>
-        <Icon.LeftIcon />
-        <span>Left</span>
-        <Icon.NotificationIcon />
-        <span>Notification</span>
-        <Icon.NotificationOffIcon />
-        <span>NotificationOff</span>
-        <Icon.PlayIcon />
-        <span>Play</span>
-        <Icon.RedoIcon />
-        <span>Redo</span>
-        <Icon.RightIcon />
-        <span>Right</span>
-        <Icon.SearchIcon />
-        <span>Search</span>
-        <Icon.SortIcon />
-        <span>Sort</span>
-        <Icon.TableIcon />
-        <span>Table</span>
-        <Icon.UnderlineIcon />
-        <span>Underline</span>
-        <Icon.UndoIcon />
-        <span>Undo</span>
-        
+      <div style={{display: 'flex', gap:'18px', flexWrap: 'wrap'}}>
+        {Object.keys(icons).map(key => {
+        const Icon = icons[key]
+        return (
+          <div key={key} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Icon />
+            <div style={{paddingTop: '6px', paddingBottom: '6px'}}>{key}</div>
+          </div>
+         
+        )
+      })}
       </div>
     </>
   );
