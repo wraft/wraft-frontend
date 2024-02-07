6870c750fd2587d2eceadfdbbdc504efd98fff41
Haroon Albar <haroon.albar@functionary.co>
2024-02-07 23:21:07 +0530

    feat(icon): updated icons

--- packages/icon/src/icons/BulletList.tsx
+++ packages/icon/src/icons/BulletList.tsx
@@ -3,12 +3,12 @@ import type { SVGProps } from 'react';
 const SvgBulletListIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={props.width || 25}
-    height={props.height || 25}
+    width={props.fontSize || props.width || 24}
+    height={props.height || props.fontSize || props.width || 24}
     fill="none"
     {...props}>
     <path
-      fill="#2C3641"
+      fill={props.color || `#2C3641`}
       d="M4.25 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5m0-6c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5m0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5m3 2.5h14v-2h-14zm0-6h14v-2h-14zm0-8v2h14V5z"
     />
   </svg>
--- packages/icon/src/icons/Up.tsx
+++ packages/icon/src/icons/Up.tsx
@@ -3,12 +3,12 @@ import type { SVGProps } from 'react';
 const SvgUpIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={props.width || 25}
-    height={props.height || 25}
+    width={props.fontSize || props.width || 24}
+    height={props.height || props.fontSize || props.width || 24}
     fill="none"
     {...props}>
     <path
-      fill="#2C3641"
+      fill={props.color || `#2C3641`}
       fillRule="evenodd"
       d="M10.99 6.704a1.43 1.43 0 0 1 2.02 0l8.572 8.572a1.429 1.429 0 1 1-2.02 2.02L12 9.735l-7.561 7.56a1.429 1.429 0 1 1-2.02-2.02z"
       clipRule="evenodd"
