0a5e0e31d638e8f24291c35ab1a7e3a613c5070a
Haroon Albar <haroon.albar@functionary.co>
2024-02-07 23:34:39 +0530

    feat(icon): updated icons

--- packages/icon/src/icons/Underline.tsx
+++ packages/icon/src/icons/Underline.tsx
@@ -3,8 +3,8 @@ import type { SVGProps } from 'react';
 const SvgUnderlineIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={props.fontSize || props.width || 24}
-    height={props.height || props.fontSize || props.width || 24}
+    width={props.width || props.fontSize || 24}
+    height={props.height || props.fontSize || 24}
     fill="none"
     {...props}>
     <path
