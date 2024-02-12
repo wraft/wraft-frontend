c8297ad4fa2cfe86387e209a2c96d7273784ec02
Haroon Albar <haroon.albar@functionary.co>
2024-02-12 11:00:58 +0530

    feat(icon): added document icon

---
+++ packages/icon/src/icons/Document.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgDocumentIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.width || 24}
+    height={props.height || 24}
+    fill="none"
+    {...props}>
+    <path
+      fill={props.color || `#2C3641`}
+      d="M20 8.94a1.3 1.3 0 0 0-.06-.27v-.09a1 1 0 0 0-.19-.28l-6-6a1 1 0 0 0-.28-.19h-.09L13.06 2H7a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V8.94m-6-3.53L16.59 8H14zM18 19a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h5v5a1 1 0 0 0 1 1h5z"
+    />
+  </svg>
+);
+export default SvgDocumentIcon;
