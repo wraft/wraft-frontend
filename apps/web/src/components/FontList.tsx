0a61eee28594df00d9e814227e7e1bc89a22cc37
Haroon Albar <haroon.albar@functionary.co>
2024-02-14 14:32:44 +0530

    feat(web): added FontList component

---
+++ apps/web/src/components/FontList.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import { DeleteIcon, DocumentIcon } from '@wraft/icon';
+import { Box, Button, Flex, Text, useThemeUI } from 'theme-ui';
+
+type Props = {
+  assets: any[];
+  onDelete?: (e: any) => void;
+};
+
+const FontList = ({ assets, onDelete }: Props) => {
+  const themeui = useThemeUI();
+  return (
+    assets &&
+    assets.length > 0 && (
+      <Box
+        sx={{
+          borderRadius: '6px',
+          overflow: 'hidden',
+          border: 'solid 1px',
+          borderColor: 'neutral.200',
+        }}>
+        {assets.map((m: any, index: number) => (
+          <Flex
+            key={m.id}
+            sx={{
+              py: 2,
+              px: 3,
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              borderBottom: index < assets.length ? '1px solid' : 'none',
+              borderColor: 'neutral.200',
+            }}>
+            <Flex sx={{ alignItems: 'center' }}>
+              <Box
+                mr={2}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <DocumentIcon
+                  viewBox="0 0 24 24"
+                  color={themeui?.theme?.colors?.gray?.[200] || '#2C3641'}
+                />
+              </Box>
+              <Text as="p" variant="pM">
+                {m.name.match(/(.+?)(?=-|$)/)?.[1]}
+              </Text>
+            </Flex>
+            <Flex
+              sx={{
+                alignItems: 'center',
+                width: '80px',
+                justifyContent: 'space-between',
+                textTransform: 'uppercase',
+              }}>
+              <Text variant="capM" sx={{ color: 'gray.400' }}>
+                {m.name.match(/-(.+?)(?=\.[^.]*$|$)/)[1]}
+              </Text>
+              {onDelete && (
+                <Button
+                  variant="base"
+                  sx={{ p: 0, m: 0 }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    onDelete(m.id);
+                  }}>
+                  <DeleteIcon
+                    width={16}
+                    height={16}
+                    viewBox="0 0 24 24"
+                    color="#2C3641"
+                  />
+                </Button>
+              )}
+            </Flex>
+          </Flex>
+        ))}
+      </Box>
+    )
+  );
+};
+
+export default FontList;
