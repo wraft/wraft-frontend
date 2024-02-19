b9016db133830dea7f259b5c0c65a734a03dceed
Haroon Albar <haroon.albar@functionary.co>
2024-02-19 11:18:30 +0530

    feat(web): flow list updated

--- apps/web/src/components/Paginate.tsx
+++ apps/web/src/components/Paginate.tsx
@@ -43,7 +43,8 @@ const StyledPaginateContainer = styled(Flex)`
 
 const Paginate = ({ total_pages, changePage, info }: IPageMeta) => {
   return (
-    <StyledPaginateContainer sx={{ fontSize: 2 }}>
+    <StyledPaginateContainer
+      sx={{ fontSize: 2, display: 'flex', alignItems: 'center' }}>
       <ReactPaginate
         pageCount={total_pages || 0}
         pageRangeDisplayed={5}
@@ -56,7 +57,13 @@ const Paginate = ({ total_pages, changePage, info }: IPageMeta) => {
         breakLabel="..."
         breakClassName="break-me"
       />
-      <Box sx={{ ml: 'auto', mr: 3, fontSize: 2, color: 'gray.300' }}>
+      <Box
+        sx={{
+          ml: 'auto',
+          mr: 3,
+          fontSize: 2,
+          color: 'gray.300',
+        }}>
         {info}
       </Box>
     </StyledPaginateContainer>
