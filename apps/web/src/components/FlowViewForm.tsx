c533ef61de8f4ccbb4557125a72c24fee7bb5c23
Haroon Albar <haroon.albar@functionary.co>
2024-02-19 13:32:15 +0530

    chore(web): cleanup FlowViewForm

--- apps/web/src/components/FlowViewForm.tsx
+++ apps/web/src/components/FlowViewForm.tsx
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import { Box, Container, Text, Flex } from 'theme-ui';
+import { Box, Container, Label } from 'theme-ui';
 
-import { postAPI, deleteAPI, fetchAPI, putAPI } from '../utils/models';
+import { fetchAPI } from '../utils/models';
 
-import { Droppable } from './Droppable';
 import Field from './Field';
 
 export interface States {
@@ -57,114 +55,28 @@ export interface StateFormProps {
   onSorted?: any;
 }
 
-interface ItemType {
-  id: number;
-  name?: string;
-  meta?: any;
-}
-
-const StatesForm = ({
-  content,
-  onDelete,
-  onAttachApproval,
-  onSorted,
-}: StateFormProps) => {
-  const [state, setState] = useState<ItemType[]>([]);
-  console.log(state);
-
-  const onDeleteFlow = (_id: any) => {
-    onDelete(_id);
-  };
-
-  const setOrder = (content: any) => {
-    if (content.size > 0) {
-      const listItems: ItemType[] = [];
-
-      content.map((c: any) => {
-        const newItemx: ItemType = { id: c?.id, name: c?.name };
-        listItems.push(newItemx);
-      });
-
-      setState(listItems);
-
-      const dbitems: any = [];
-
-      listItems.map((dbi: any, index) => {
-        dbitems.push({ id: dbi.id, order: index });
-      });
-
-      onSorted(dbitems);
-    }
-  };
-
-  useEffect(() => {
-    if (content) {
-      const listItems: ItemType[] = [];
-      content.map((c: any) => {
-        const newItemx: ItemType = {
-          id: c?.state.id,
-          name: c?.state.state,
-          meta: state,
-        };
-        listItems.push(newItemx);
-      });
-
-      setState(listItems);
-    }
-  }, [content]);
-
-  return (
-    <Box p={0}>
-      <Box sx={{ pb: '12px', pt: '32px' }}>
-        <Text variant="sectiontitle" sx={{ mb: 0, mt: 0, color: 'grey' }}>
-          Flow states
-        </Text>
-      </Box>
-      {content && (
-        <Box
-          mb={0}
-          sx={{
-            border: '1px solid #E4E9EF',
-            borderRadius: '4px 4px 4px 4px',
-          }}>
-          <Droppable
-            list={state}
-            setList={setOrder}
-            onAttachApproval={onAttachApproval}
-            onDeleteFlow={onDeleteFlow}></Droppable>
-        </Box>
-      )}
-    </Box>
-  );
-};
-
 const FlowViewForm = () => {
   const { register, setValue } = useForm();
-  const [edit, setEdit] = useState<boolean>(false);
-  const [approval, setApproval] = useState<boolean>(false);
-  const [content, setContent] = useState<StateElement[]>();
+  // const [edit, setEdit] = useState<boolean>(false);
+  // const [content, setContent] = useState<StateElement[]>();
   const [flow, setFlow] = useState<Flow>();
+  const [states, setStates] = useState<any>();
 
   // determine edit state based on URL
   const router = useRouter();
   const cId: string = router.query.id as string;
 
-  const onAttachApproval = (_d: any) => {
-    setApproval(!approval);
-    console.log('onAttachApproval', _d);
-  };
-
-  /**
-   * Load all states for a particular Flow
-   * @param id flow id
-   * @param t  token
-   */
-  const loadStates = (id: string) => {
-    fetchAPI(`flows/${id}/states`).then((data: any) => {
-      const res: States = data;
-      setContent(res.states);
-    });
-  };
+  // /**
+  //  * Load all states for a particular Flow
+  //  * @param id flow id
+  //  * @param t  token
+  //  */
+  // const loadStates = (id: string) => {
+  //   fetchAPI(`flows/${id}/states`).then((data: any) => {
+  //     const res: States = data;
+  //     setContent(res.states);
+  //   });
+  // };
 
   /**
    * Load all states for a particular Flow
@@ -173,87 +85,49 @@ const FlowViewForm = () => {
    */
   const loadFlow = (fId: string) => {
     fetchAPI(`flows/${fId}`).then((data: any) => {
+      console.log('flowwwwwwwwww', data);
       const res: Flow = data.flow;
       setFlow(res);
+      const states: States = data.states;
+      setStates(states);
       setValue('name', res?.name);
-    });
-  };
-
-  /**
-   * Create State
-   * @param data Form Data
-   */
-  const CreateState = (data: any) => {
-    postAPI(`flows/${cId}/states`, data).then(() => {
-      if (cId) {
-        loadStates(cId);
-      }
-    });
-  };
-
-  /**
-   * Delete State
-   * @param data Form Data
-   */
-  const deleteState = (fId: any) => {
-    deleteAPI(`states/${fId}`).then(() => {
-      toast.success('Deleted a flow', {
-        duration: 1000,
-        position: 'top-right',
-      });
-      loadStates(cId);
     });
   };
 
   useEffect(() => {
     if (cId && cId.length > 0) {
-      setEdit(true);
-      loadStates(cId);
+      // setEdit(true);
+      // loadStates(cId);
       loadFlow(cId);
     }
   }, [cId]);
 
-  const onSortDone = (data: any) => {
-    const formative = {
-      states: data,
-    };
-
-    putAPI(`/flows/${cId}/align-states`, formative).then(() => {
-      toast.success('Sorted flow state', {
-        duration: 1000,
-        position: 'top-right',
-      });
-    });
-  };
-
   return (
     <Box sx={{ px: 4, pb: 3, backgroundColor: 'white', width: '556px' }}>
-      <Box>
-        <Container data-flow={flow?.id}>
-          <Box as="form">
-            <Flex>
-              <Box sx={{ pt: '30px', width: '492px' }}>
-                <Field
-                  name="name"
-                  label="Name"
-                  defaultValue=""
-                  register={register}
-                />
-              </Box>
-            </Flex>
-
-            {edit && content && (
-              <StatesForm
-                onAttachApproval={onAttachApproval}
-                content={content}
-                onSave={CreateState}
-                onDelete={deleteState}
-                onSorted={onSortDone}
-              />
-            )}
+      <Container data-flow={flow?.id}>
+        <Box as="form">
+          <Box sx={{ pt: 4, width: '100%' }}>
+            <Field
+              name="name"
+              label="Name"
+              defaultValue=""
+              register={register}
+            />
           </Box>
-        </Container>
-      </Box>
+          <Box pt={4}>
+            <Label>Flow states</Label>
+            {states &&
+              states.map((item: any, index: number) => {
+                return <Box key={index}>{item.state}</Box>;
+              })}
+          </Box>
+
+          {/* {edit &&
+            content?.map((item: any, index: number) => (
+              <Box key={index}>{item}</Box>
+            ))} */}
+        </Box>
+      </Container>
     </Box>
   );
 };
