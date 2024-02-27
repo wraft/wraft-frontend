d82ef167a16888f74ac25178296df602e2151105
Haroon Albar <haroon.albar@functionary.co>
2024-02-27 12:55:09 +0530

    feat(web): made a working modal

--- apps/web/src/components/FieldEditorNew.tsx
+++ apps/web/src/components/FieldEditorNew.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
-import { Button, Box, Flex, Text, Spinner } from 'theme-ui';
+import { Button, Box, Flex, Text, Spinner, useThemeUI } from 'theme-ui';
 import { Label, Select } from 'theme-ui';
+import { CloseIcon } from '@wraft/icon';
 
-import { Trash } from './Icons';
 import Field from './Field';
+import Modal from './Modal';
 
 interface FieldFormProps {
   fields?: any;
@@ -25,6 +26,9 @@ type FieldValues = {
 
 const FieldForm = (props: FieldFormProps) => {
   const fieldsArr = useMemo(() => props.fields || [], [props.fields]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const themeui = useThemeUI();
 
   const {
     register,
@@ -50,9 +54,9 @@ const FieldForm = (props: FieldFormProps) => {
 
   const [submitting, setSubmitting] = useState<boolean>(false);
 
-  const onSubmit = (data: FieldValues) => {
+  const onSubmit = () => {
     setSubmitting(true);
-    const vals = getValues();
+    const vals: FieldValues = getValues();
     console.log('submit on FieldEditor', vals);
 
     const filteredVals = vals.fields.filter(
@@ -69,160 +73,189 @@ const FieldForm = (props: FieldFormProps) => {
 
     props.onSave(results);
     setSubmitting(false);
+    setIsOpen(false);
   };
 
   return (
     <Box>
-      <Box as="form" onSubmit={handleSubmit(onSubmit)}>
-        {fields.length < 1 && fieldsArr.length < 1 && (
-          <Box
-            sx={{
-              bg: 'neutral.200',
-              pt: 4,
-              pb: 4,
-              px: 4,
-              alignItems: 'flex-start',
-            }}>
-            <Text as="h3" sx={{ mb: 0 }}>
-              Empty State
-            </Text>
-            <Text as="p" sx={{ fontSize: 1, color: 'text', mb: 3, pr: 3 }}>
-              You have no fields added, start adding fields to `field` up your
-              Variant
-            </Text>
-            <Button
-              type="button"
-              variant="btnPrimary"
-              onClick={(e) => {
-                e.preventDefault();
-                append({ name: '', type: '' });
-              }}>
-              Add Field
-            </Button>
-          </Box>
-        )}
-        <Box>
-          {fields.map((field, index) => (
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <Box
+          as="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ minWidth: '60ch', p: 4 }}>
+          {fields.length < 1 && fieldsArr.length < 1 && (
             <Box
-              key={field.id}
               sx={{
-                borderBottom:
-                  index === fields.length - 1 ? 'none' : '1px solid',
-                borderColor: 'border',
-                pb: index === fields.length - 1 ? 0 : 4,
-                pt: index === 0 ? 0 : 4,
+                bg: 'neutral.200',
+                pt: 4,
+                pb: 4,
+                px: 4,
+                alignItems: 'flex-start',
               }}>
-              <Flex sx={{ alignItems: 'center' }}>
-                <Field
-                  name={`fields.${index}.name`}
-                  label="Field Name"
-                  register={register}
-                  defaultValue={(field && field.name) || ''}
-                  onChange={() => handleSubmit(onSubmit)()}
-                  disable={
-                    props.content &&
-                    field &&
-                    !props.content?.content_type.fields.every(
-                      (f: any) => f.name !== field.name,
-                    )
-                  }
-                  error={errors.fields?.[index]?.name}
-                />
-                <Box sx={{ flexGrow: 1, ml: 3 }}>
-                  <Label htmlFor="`fields[${idx}][type]`" mb={1}>
-                    Field Type
-                  </Label>
-                  <Select
-                    disabled={
+              <Text as="h3" sx={{ mb: 0 }}>
+                Empty State
+              </Text>
+              <Text as="p" sx={{ fontSize: 1, color: 'text', mb: 3, pr: 3 }}>
+                You have no fields added, start adding fields to `field` up your
+                Variant
+              </Text>
+              <Button
+                type="button"
+                variant="btnPrimary"
+                onClick={(e) => {
+                  e.preventDefault();
+                  append({ name: '', type: '' });
+                }}>
+                Add Field
+              </Button>
+            </Box>
+          )}
+          <Box>
+            {fields.map((field, index) => (
+              <Box
+                key={field.id}
+                sx={{
+                  borderBottom:
+                    index === fields.length - 1 ? 'none' : '1px solid',
+                  borderColor: 'border',
+                  pb: index === fields.length - 1 ? 0 : 4,
+                  pt: index === 0 ? 0 : 4,
+                }}>
+                <Flex sx={{ alignItems: 'center' }}>
+                  <Field
+                    name={`fields.${index}.name`}
+                    label="Field Name"
+                    register={register}
+                    defaultValue={(field && field.name) || ''}
+                    onChange={() => handleSubmit(onSubmit)()}
+                    disable={
                       props.content &&
                       field &&
                       !props.content?.content_type.fields.every(
                         (f: any) => f.name !== field.name,
                       )
                     }
-                    defaultValue={(field && field.type) || ''}
-                    {...register(`fields.${index}.type` as const, {
-                      required: true,
-                    })}
-                    onChange={() => handleSubmit(onSubmit)()}>
-                    <option disabled selected value={''}>
-                      select an option
-                    </option>
-                    {props.fieldtypes &&
-                      props.fieldtypes.length > 0 &&
-                      props.fieldtypes.map((m: any) => (
-                        <option value={m.id} key={m.id}>
-                          {m.name}
-                        </option>
-                      ))}
-                  </Select>
-                </Box>
-                <Box sx={{ ml: 2, textAlign: 'right' }}>
-                  <Button
-                    // variant="buttonSecondary"
-                    variant="base"
-                    type="button"
-                    sx={{
-                      mt: '28px',
-                      p: 2,
-                      flexShrink: 0,
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                    onClick={() => {
-                      props.removeField(index);
-                      remove(index);
-                    }}>
-                    <Trash color="red" width={24} height={24} />
-                  </Button>
-                </Box>
-              </Flex>
-            </Box>
-          ))}
-        </Box>
-        {(fields?.length > 0 || fieldsArr.length > 0) && (
-          <Box
-            sx={{
-              alignItems: 'flex-start',
-            }}>
-            <Button
-              variant="buttonSecondary"
-              type="button"
-              mt={4}
-              onClick={(e) => {
-                e.preventDefault();
-                append({ name: '', type: '' });
-              }}>
-              Add Field
-            </Button>
+                    error={errors.fields?.[index]?.name}
+                  />
+                  <Box sx={{ flexGrow: 1, ml: 3 }}>
+                    <Label htmlFor="`fields[${idx}][type]`" mb={1}>
+                      Field Type
+                    </Label>
+                    <Select
+                      disabled={
+                        props.content &&
+                        field &&
+                        !props.content?.content_type.fields.every(
+                          (f: any) => f.name !== field.name,
+                        )
+                      }
+                      defaultValue={(field && field.type) || ''}
+                      {...register(`fields.${index}.type` as const, {
+                        required: true,
+                      })}
+                      onChange={() => handleSubmit(onSubmit)()}>
+                      <option disabled selected value={''}>
+                        select an option
+                      </option>
+                      {props.fieldtypes &&
+                        props.fieldtypes.length > 0 &&
+                        props.fieldtypes.map((m: any) => (
+                          <option value={m.id} key={m.id}>
+                            {m.name}
+                          </option>
+                        ))}
+                    </Select>
+                  </Box>
+                  <Box sx={{ ml: 2, textAlign: 'right' }}>
+                    <Button
+                      // variant="buttonSecondary"
+                      variant="base"
+                      type="button"
+                      sx={{
+                        mt: '28px',
+                        p: 2,
+                        flexShrink: 0,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                      onClick={() => {
+                        props.removeField(index);
+                        remove(index);
+                      }}>
+                      {/* <Trash color="red" width={24} height={24} /> */}{' '}
+                      <CloseIcon
+                        color={themeui?.theme?.colors?.gray?.[600]}
+                        width={18}
+                        height={18}
+                      />
+                    </Button>
+                  </Box>
+                </Flex>
+              </Box>
+            ))}
           </Box>
-        )}
-        {(fields?.length > 0 || fields?.length > 0) && (
-          <Flex mt={4}>
-            <Button
-              variant="btnPrimaryLarge"
-              sx={{ ml: 1 }}
-              type="submit"
-              onClick={(e) => {
-                e.preventDefault();
-                handleSubmit(onSubmit)();
+          {(fields?.length > 0 || fieldsArr.length > 0) && (
+            <Box
+              sx={{
+                alignItems: 'flex-start',
               }}>
-              {submitting && <Spinner color="white" width={24} />}
-              Save
-            </Button>
-          </Flex>
-        )}
-      </Box>
+              <Button
+                variant="buttonSecondary"
+                type="button"
+                mt={4}
+                onClick={(e) => {
+                  e.preventDefault();
+                  append({ name: '', type: '' });
+                }}>
+                Add Field
+              </Button>
+            </Box>
+          )}
+          {(fields?.length > 0 || fields?.length > 0) && (
+            <Flex sx={{ justifyContent: 'space-between' }} mt={4}>
+              <Box />
+              <Button
+                variant="btnPrimaryLarge"
+                sx={{ ml: 1 }}
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleSubmit(onSubmit)();
+                }}>
+                {submitting && <Spinner color="white" width={24} />}
+                Save
+              </Button>
+            </Flex>
+          )}
+        </Box>
+      </Modal>
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          setIsOpen(true);
+        }}
+        variant="buttonSecondary">
+        Edit
+      </Button>
       <Box
         sx={{
           mt: 4,
+          border: 'solid 1px',
+          borderColor: 'border',
+          borderRadius: '4px',
         }}>
         {fieldsArr &&
-          fieldsArr.map((f: any) => (
+          fieldsArr.map((f: any, index: number) => (
             <Flex
               key={f?.id}
-              sx={{ py: 2, p: 2, border: 'solid 1px', borderColor: 'border' }}>
+              sx={{
+                py: 2,
+                p: 2,
+                borderBottom:
+                  index === fieldsArr.length - 1 ? 'none' : '1px solid',
+                borderColor: 'border',
+              }}>
               <Text as="h4">{(f && f.value && f.value.name) || ''}</Text>
               <Text
                 variant="caps"
