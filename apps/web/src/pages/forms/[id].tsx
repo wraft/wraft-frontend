752ed16254291036563481c611bd3dc96ef79077
Haroon Albar <haroon.albar@functionary.co>
2024-04-05 10:30:56 +0530

    feat(web): add menu step indicator

--- apps/web/src/pages/forms/[id].tsx
+++ apps/web/src/pages/forms/[id].tsx
@@ -1,14 +1,20 @@
 import { FC, useState } from 'react';
 import Head from 'next/head';
-import { Box, Container, Flex } from 'theme-ui';
+import { Box, Container, Flex, Text } from 'theme-ui';
 
 import Page from 'components/PageFrame';
 import FormsFrom from 'components/FormsFrom';
 import PageHeader from 'components/PageHeader';
 import FormFieldDroppable from 'components/FormFieldDroppable';
+import MenuStepsIndicator from 'components/MenuStepsIndicator';
 
 const Index: FC = () => {
   const [items, setItems] = useState<any>([]);
+  const [formStep, setFormStep] = useState<number>(0);
+
+  const goTo = (step: number) => {
+    setFormStep(step);
+  };
   return (
     <>
       <Head>
@@ -23,13 +29,41 @@ const Index: FC = () => {
         <Flex>
           <Container variant="layout.pageFrame">
             <Flex>
-              {/* <ManageSidebar items={menuLinks} /> */}
-              <Box sx={{ width: '100%', bg: 'white' }}>
+              <MenuStepsIndicator
+                formStep={formStep}
+                goTo={goTo}
+                titles={['Questions', 'Responses']}
+              />
+              <Box
+                sx={{
+                  display: formStep === 0 ? 'block' : 'none',
+                  width: '100%',
+                  bg: 'white',
+                }}>
                 <FormsFrom items={items} setItems={setItems} />
+              </Box>
+              <Box
+                sx={{
+                  display: formStep === 1 ? 'block' : 'none',
+                  width: '100%',
+                  bg: 'white',
+                }}>
+                <Flex
+                  sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
+                  }}>
+                  <Text variant="h4Bold">Analytics</Text>
+                </Flex>
               </Box>
             </Flex>
           </Container>
-          <Box sx={{ minWidth: '349px' }}>
+          <Box
+            sx={{
+              minWidth: '349px',
+              display: formStep === 0 ? 'block' : 'none',
+            }}>
             <Box sx={{ p: '24px' }}>
               <FormFieldDroppable items={items} setItems={setItems} />
             </Box>
