c71c615a4c1c35156a138f7e9f641226cbdfdde4
thalha <thalhaz999@gmail.com>
2024-08-09 12:55:55 +0530

    fix: block delete confirmation modal

--- apps/web/src/components/BlockTemplateList.tsx
+++ apps/web/src/components/BlockTemplateList.tsx
@@ -1,13 +1,18 @@
 import React, { FC, useEffect, useState } from 'react';
+import Router from 'next/router';
 import NavLink from 'next/link';
 import { Table, DropdownMenu } from '@wraft/ui';
 import { Box, Text, Flex, Avatar } from 'theme-ui';
 import { ThreeDotIcon } from '@wraft/icon';
+import toast from 'react-hot-toast';
 
 import { TimeAgo } from 'components/Atoms';
 import Link from 'components/NavLink';
 import PageHeader from 'components/PageHeader';
-import { fetchAPI } from 'utils/models';
+import { deleteAPI, fetchAPI } from 'utils/models';
+
+import Modal from './Modal';
+import { ConfirmDelete } from './common';
 
 export interface IField {
   id: string;
@@ -21,74 +26,13 @@ export interface IFieldItem {
   type: string;
 }
 
-const columns = [
-  {
-    id: 'title',
-    header: 'Name',
-    accessorKey: 'title',
-    cell: ({ row }: any) => (
-      <NavLink href={`/blocks/edit/${row?.original?.id}`}>
-        <Box sx={{ fontSize: 'sm', fontWeight: 500 }}>
-          {row?.original?.title}
-        </Box>
-      </NavLink>
-    ),
-    size: '350',
-    enableSorting: false,
-  },
-  {
-    id: 'content.updated_at',
-    header: 'TIME',
-    accessorKey: 'TIME',
-    cell: ({ row }: any) => (
-      <Box>
-        <TimeAgo time={row.original?.updated_at} />
-      </Box>
-    ),
-    enableSorting: false,
-  },
-  {
-    id: 'content.created',
-    header: 'CREATED BY',
-    accessorKey: 'created',
-    cell: ({ row }: any) => (
-      <Flex>
-        <Avatar width="20px" src={row.original?.creator?.profile_pic} />
-        <Box sx={{ fontSize: 'sm', ml: 3 }}>{row.original?.creator?.name}</Box>
-      </Flex>
-    ),
-    enableSorting: false,
-  },
-  {
-    id: 'content.name',
-    header: '',
-    cell: ({ row }: any) => (
-      <>
-        <Flex sx={{ justifyContent: 'flex-end' }}>
-          <DropdownMenu.Provider>
-            <DropdownMenu.Trigger>
-              <ThreeDotIcon />
-            </DropdownMenu.Trigger>
-            <DropdownMenu aria-label="dropdown role">
-              <DropdownMenu.Item>
-                <NavLink href={`/blocks/edit/${row?.original?.id}`}>
-                  <Text>Edit</Text>
-                </NavLink>
-              </DropdownMenu.Item>
-              <DropdownMenu.Item>Delete</DropdownMenu.Item>
-            </DropdownMenu>
-          </DropdownMenu.Provider>
-        </Flex>
-      </>
-    ),
-    enableSorting: false,
-    textAlign: 'right',
-  },
-];
-
 const BlockTemplateListFrame: FC = () => {
   const [contents, setContents] = useState<any>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [render, setRender] = useState(false);
+  const [isOpen, setIsOpen] = useState<number | null>(null);
+  const [deleteBlock, setDeleteBlock] = useState<number | null>(null);
+
   const loadData = () => {
     setLoading(true);
     fetchAPI('block_templates')
@@ -102,9 +46,110 @@ const BlockTemplateListFrame: FC = () => {
       });
   };
 
+  const blockDelete = (cId: any) => {
+    if (cId) {
+      deleteAPI(`block_templates/${cId}`).then(() => {
+        Router.push(`/blocks`);
+        toast.success('Deleted Block Successfully', {
+          duration: 1000,
+          position: 'top-right',
+        });
+        setRender((prev: boolean) => !prev);
+        setDeleteBlock(null);
+      });
+    }
+  };
+
   useEffect(() => {
     loadData();
-  }, []);
+  }, [render]);
+
+  const columns = [
+    {
+      id: 'title',
+      header: 'Name',
+      accessorKey: 'title',
+      cell: ({ row }: any) => (
+        <NavLink href={`/blocks/edit/${row?.original?.id}`}>
+          <Box sx={{ fontSize: 'sm', fontWeight: 500 }}>
+            {row?.original?.title}
+          </Box>
+        </NavLink>
+      ),
+      size: '350',
+      enableSorting: false,
+    },
+    {
+      id: 'content.updated_at',
+      header: 'TIME',
+      accessorKey: 'TIME',
+      cell: ({ row }: any) => (
+        <Box>
+          <TimeAgo time={row.original?.updated_at} />
+        </Box>
+      ),
+      enableSorting: false,
+    },
+    {
+      id: 'content.created',
+      header: 'CREATED BY',
+      accessorKey: 'created',
+      cell: ({ row }: any) => (
+        <Flex>
+          <Avatar width="20px" src={row.original?.creator?.profile_pic} />
+          <Box sx={{ fontSize: 'sm', ml: 3 }}>
+            {row.original?.creator?.name}
+          </Box>
+        </Flex>
+      ),
+      enableSorting: false,
+    },
+    {
+      id: 'content.name',
+      header: '',
+      cell: ({ row }: any) => (
+        <>
+          <Flex sx={{ justifyContent: 'flex-end' }}>
+            <DropdownMenu.Provider>
+              <DropdownMenu.Trigger>
+                <ThreeDotIcon />
+              </DropdownMenu.Trigger>
+              <DropdownMenu aria-label="dropdown role">
+                <DropdownMenu.Item>
+                  <NavLink href={`/blocks/edit/${row?.original?.id}`}>
+                    <Text>Edit</Text>
+                  </NavLink>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item
+                  onClick={() => {
+                    setIsOpen(null);
+                    setDeleteBlock(row.index);
+                  }}>
+                  Delete
+                </DropdownMenu.Item>
+                <Modal
+                  isOpen={deleteBlock === row.index}
+                  onClose={() => setDeleteBlock(null)}>
+                  {
+                    <ConfirmDelete
+                      title="Delete Variant"
+                      text={`Are you sure you want to delete ‘${row?.original?.title}’?`}
+                      setOpen={setDeleteBlock}
+                      onConfirmDelete={async () => {
+                        blockDelete(row.original.id);
+                      }}
+                    />
+                  }
+                </Modal>
+              </DropdownMenu>
+            </DropdownMenu.Provider>
+          </Flex>
+        </>
+      ),
+      enableSorting: false,
+      textAlign: 'right',
+    },
+  ];
 
   return (
     <Box>
