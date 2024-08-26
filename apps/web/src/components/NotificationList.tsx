5f5e73044e8723890f938e37b7d27499572c26a6
thalha <thalhaz999@gmail.com>
2024-08-26 10:31:48 +0530

    feat: pagination for notification

--- apps/web/src/components/NotificationList.tsx
+++ apps/web/src/components/NotificationList.tsx
@@ -1,30 +1,63 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { Box } from 'theme-ui';
-import { Table } from '@wraft/ui';
+import { Pagination, Table } from '@wraft/ui';
 
 import { fetchAPI } from 'utils/models';
 
 import { TimeAgo } from './Atoms';
 
+export interface IPageMeta {
+  page_number: number;
+  total_entries: number;
+  total_pages: number;
+  contents?: any;
+}
+
 const NotificationList = () => {
   const [contents, setContents] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [pageMeta, setPageMeta] = useState<IPageMeta>();
+  const [page, setPage] = useState<number>(1);
+
+  const router: any = useRouter();
+  const currentPage: any = parseInt(router.query.page) || 1;
 
   useEffect(() => {
-    loadData();
+    if (page) {
+      loadData();
+    }
   }, []);
 
   const loadData = () => {
     setLoading(true);
-    fetchAPI('notifications')
+    const pageNo = currentPage ? `&page=${currentPage}` : '';
+
+    const query = `sort=inserted_at_desc${pageNo}`;
+    fetchAPI(`notifications?${query}`)
       .then((data: any) => {
         setLoading(false);
         const res = data.notifications;
         setContents(res);
+        setPageMeta(data);
       })
       .catch(() => {
         setLoading(false);
       });
+  };
+
+  const changePage = (newPage: any) => {
+    setPage(newPage);
+    const currentPath = router.pathname;
+    const currentQuery = { ...router.query, page: newPage };
+    router.push(
+      {
+        pathname: currentPath,
+        query: currentQuery,
+      },
+      undefined,
+      { shallow: true },
+    );
   };
 
   const columns = [
@@ -79,6 +112,16 @@ const NotificationList = () => {
                 isLoading={loading}
                 emptyMessage="No Notifications yet."
               />
+              {pageMeta && pageMeta?.total_pages > 1 && (
+                <Box mx={0} mt={3}>
+                  <Pagination
+                    totalPage={pageMeta?.total_pages}
+                    initialPage={currentPage}
+                    onPageChange={changePage}
+                    totalEntries={pageMeta?.total_entries}
+                  />
+                </Box>
+              )}
             </Box>
           </Box>
         </Box>
