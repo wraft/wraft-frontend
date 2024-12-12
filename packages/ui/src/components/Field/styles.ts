b91aec480dd2c64943f696539748d5b34ac43f85
Salsabeel <k.salsabeel@gmail.com>
2024-12-12 10:37:24 +0530

    feat(ui): add field

---
+++ packages/ui/src/components/Field/styles.ts
@@ -0,0 +1,36 @@
+import styled, { css, system, th } from "@xstyled/emotion";
+
+import { Variant } from "./index";
+
+export const Field = styled.div<any>`
+  position: relative;
+`;
+
+export const VARIANTS: Record<Variant, string> = {
+  error: "danger-500",
+  info: "info-500",
+  success: "success-500",
+  warning: "warning-500",
+};
+
+export const Hint = styled.div<{ variant: Variant }>`
+  display: flex;
+  ${({ variant }) => css`
+    color: ${VARIANTS[variant] || undefined};
+  `}
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  margin-top: 4px;
+`;
+
+export const Label = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: sm;
+`;
+
+export const LabelWithHint = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
