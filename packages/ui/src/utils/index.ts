bd3e822c1c184db86350dadd6557dd6315c85410
Salsabeel <k.salsabeel@gmail.com>
2024-12-27 19:03:48 +0530

    feat:(ui): add tag ui component

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -3,3 +3,6 @@ export * from "./field-styles";
 export * from "./create-event";
 export * from "./use-isomorphic-layout-effect";
 export * from "./wrap-children";
+export * from "./overflow-ellipsis";
+export * from "./throttle";
+export * from "./hide-focus-rings-root";
---
+++ packages/ui/src/utils/overflow-ellipsis.ts
@@ -0,0 +1,7 @@
+import { css } from "@xstyled/emotion";
+
+export const overflowEllipsis = css`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+` as any;
