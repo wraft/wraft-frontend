baacee6928ea3c27a08b438555fb3eb82f1002a6
Salsabeel <k.salsabeel@gmail.com>
2024-12-31 21:25:09 +0530

    feat:(ui): add flex ui component

---
+++ packages/ui/src/components/Flex/index.tsx
@@ -0,0 +1,60 @@
+import { Box } from "../Box";
+
+import { CreateWuiProps, forwardRef, WuiProps } from "@/system";
+
+export interface FlexOptions {
+  /** same as alignItems */
+  align?: WuiProps["alignItems"];
+  /** same as flexBasis */
+  basis?: WuiProps["flexBasis"];
+  /** same as flexDirection */
+  direction?: WuiProps["flexDirection"];
+  /** same as flexGrow */
+  grow?: WuiProps["flexGrow"];
+  /** same as justifyContent */
+  justify?: WuiProps["justifyContent"];
+  /** same as flexShrink */
+  shrink?: WuiProps["flexShrink"];
+  /** same as flexWrap */
+  wrap?: WuiProps["flexWrap"];
+  variant?: string;
+}
+
+export type FlexProps = CreateWuiProps<"div", FlexOptions>;
+
+export const Flex = forwardRef<"div", FlexProps>(
+  (
+    {
+      align,
+      basis,
+      dataTestId,
+      direction,
+      grow,
+      justify,
+      shrink,
+      wrap,
+      variant,
+      ...rest
+    },
+    ref,
+  ) => {
+    return (
+      <Box
+        alignItems={align}
+        data-testid={dataTestId}
+        display="flex"
+        flexBasis={basis}
+        flexDirection={direction}
+        flexGrow={grow}
+        flexShrink={shrink}
+        flexWrap={wrap}
+        justifyContent={justify}
+        variant={variant}
+        ref={ref}
+        {...rest}
+      />
+    );
+  },
+);
+
+Flex.displayName = "Flex";
