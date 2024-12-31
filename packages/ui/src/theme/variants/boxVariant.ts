0c3063a740c6afc83fc0cf42f00b38ceeeb68ede
Salsabeel <k.salsabeel@gmail.com>
2024-12-31 21:27:35 +0530

    feat:(ui): add box variants

---
+++ packages/ui/src/theme/variants/boxVariant.ts
@@ -0,0 +1,13 @@
+const boxVariant = {
+  boxone: {
+    width: "100%",
+    borderCollapse: "separate",
+    borderSpacing: 0,
+  },
+  boxtwo: {
+    backgroundColor: "red.100",
+    textAlign: "center",
+  },
+};
+
+export default boxVariant;
