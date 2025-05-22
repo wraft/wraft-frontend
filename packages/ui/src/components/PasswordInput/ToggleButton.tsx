ccae0975e1d2707e5959730160daee18574aa0be
shahid <mrshahid234@gmail.com>
2025-05-22 15:19:56 +0530

    chore(web):  replace duplicate hide icon with show icon

--- packages/ui/src/components/PasswordInput/ToggleButton.tsx
+++ packages/ui/src/components/PasswordInput/ToggleButton.tsx
@@ -28,9 +28,9 @@ export const ToggleButton: React.FC<ToggleButtonProps> = ({
       variant="ghost"
     >
       {isHidden ? (
-        <ShowIcon width="18px" height="18px" />
-      ) : (
         <HideIcon width="18px" height="18px" />
+      ) : (
+        <ShowIcon width="18px" height="18px" />
       )}
     </Button>
   );
