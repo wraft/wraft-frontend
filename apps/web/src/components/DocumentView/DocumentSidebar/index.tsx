a061c4b2895c98387c5c26c35bb542dd3f8f4aa4
Salsabeel <k.salsabeel@gmail.com>
2025-05-23 20:10:29 +0530

    chore: gust sign flow

--- apps/web/src/components/DocumentView/DocumentSidebar/index.tsx
+++ apps/web/src/components/DocumentView/DocumentSidebar/index.tsx
@@ -24,7 +24,9 @@ export const DocumentSidebar = () => {
   } = useDocument();
   const { canAccess } = usePermissions(userType, docRole);
 
-  const tab = useTab({ defaultSelectedId: 'overview' });
+  const tab = useTab({
+    defaultSelectedId: docRole === 'signer' ? 'signers' : 'overview',
+  });
   const selectedId = tab.useState('selectedId');
 
   useEffect(() => {
@@ -48,95 +50,107 @@ export const DocumentSidebar = () => {
         />
       </Box>
 
-      <Box>
-        <Tab.List aria-label="Content Tab" store={tab}>
-          <Tab id="overview" store={tab}>
-            Overview
-          </Tab>
+      {docRole === 'signer' && (
+        <Box mt="md" px="md">
+          <SignerBlock />
+        </Box>
+      )}
 
-          {canAccess('comment') && (
-            <Tab id="discussions" store={tab}>
-              Discussions
-            </Tab>
-          )}
-          {canAccess('history') && (
-            <Tab id="versions" store={tab}>
-              Versions
-            </Tab>
-          )}
-          {canAccess('log') && (
-            <Tab id="log" store={tab}>
-              Log
-            </Tab>
-          )}
-          {canAccess('sign') && (
-            <Tab id="signers" store={tab}>
-              Signers
-            </Tab>
-          )}
-        </Tab.List>
+      {docRole !== 'signer' && (
+        <Box>
+          <Tab.List aria-label="Content Tab" store={tab}>
+            {canAccess('overview') && (
+              <Tab id="overview" store={tab}>
+                Overview
+              </Tab>
+            )}
 
-        <Box h="calc(100vh - 142px)" overflowY="auto">
-          <Tab.Panel tabId="overview" store={tab}>
-            <InfoSection />
-          </Tab.Panel>
-          {canAccess('comment') && (
-            <Tab.Panel tabId="discussions" store={tab}>
-              <Box mt="md" px="md">
-                <Box>
-                  <Box mb="sm">
-                    <Text as="h3">Discussions</Text>
+            {canAccess('comment') && (
+              <Tab id="discussions" store={tab}>
+                Discussions
+              </Tab>
+            )}
+            {canAccess('history') && (
+              <Tab id="versions" store={tab}>
+                Versions
+              </Tab>
+            )}
+            {canAccess('log') && (
+              <Tab id="log" store={tab}>
+                Log
+              </Tab>
+            )}
+            {canAccess('sign') && (
+              <Tab id="signers" store={tab}>
+                Signers
+              </Tab>
+            )}
+          </Tab.List>
+
+          <Box h="calc(100vh - 142px)" overflowY="auto">
+            {canAccess('overview') && (
+              <Tab.Panel tabId="overview" store={tab}>
+                <InfoSection />
+              </Tab.Panel>
+            )}
+            {canAccess('comment') && (
+              <Tab.Panel tabId="discussions" store={tab}>
+                <Box mt="md" px="md">
+                  <Box>
+                    <Box mb="sm">
+                      <Text as="h3">Discussions</Text>
+                    </Box>
+
+                    {contents && contents.content && (
+                      <CommentForm
+                        master={contents.content_type.id}
+                        master_id={contents.content.id}
+                      />
+                    )}
                   </Box>
-
-                  {contents && contents.content && (
-                    <CommentForm
-                      master={contents.content_type.id}
-                      master_id={contents.content.id}
-                    />
+                </Box>
+              </Tab.Panel>
+            )}
+            {canAccess('history') && (
+              <Tab.Panel tabId="versions" store={tab}>
+                <Box mt="md" px="md">
+                  {contents?.versions && contents?.versions.length > 0 && (
+                    <Box>
+                      {contents.versions.map((v: any) => (
+                        <Flex key={v?.id} justify="space-between" py="sm">
+                          <Text>Version {v?.version_number}</Text>
+                          <Box>
+                            <TimeAgo time={v?.updated_at} />
+                          </Box>
+                        </Flex>
+                      ))}
+                    </Box>
                   )}
                 </Box>
-              </Box>
-            </Tab.Panel>
-          )}
-          {canAccess('history') && (
-            <Tab.Panel tabId="versions" store={tab}>
-              <Box mt="md" px="md">
-                {contents?.versions && contents?.versions.length > 0 && (
+              </Tab.Panel>
+            )}
+            {canAccess('log') && (
+              <Tab.Panel tabId="log" store={tab}>
+                <Box mt="md" px="md">
+                  <Text as="h5" mb="sm">
+                    Approval Log
+                  </Text>
                   <Box>
-                    {contents.versions.map((v: any) => (
-                      <Flex key={v?.id} justify="space-between" py="sm">
-                        <Text>Version {v?.version_number}</Text>
-                        <Box>
-                          <TimeAgo time={v?.updated_at} />
-                        </Box>
-                      </Flex>
-                    ))}
+                    {tabActiveId === 'log' && <ApprovalFlowHistory id={cId} />}
                   </Box>
-                )}
-              </Box>
-            </Tab.Panel>
-          )}
-          {canAccess('log') && (
-            <Tab.Panel tabId="log" store={tab}>
-              <Box mt="md" px="md">
-                <Text as="h5" mb="sm">
-                  Approval Log
-                </Text>
-                <Box>
-                  {tabActiveId === 'log' && <ApprovalFlowHistory id={cId} />}
                 </Box>
-              </Box>
-            </Tab.Panel>
-          )}
-          {canAccess('sign') && (
-            <Tab.Panel tabId="signers" store={tab}>
-              <Box mt="md" px="md">
-                <SignerBlock />
-              </Box>
-            </Tab.Panel>
-          )}
+              </Tab.Panel>
+            )}
+            {canAccess('sign') && (
+              <Tab.Panel tabId="signers" store={tab}>
+                <Box mt="md" px="md">
+                  <SignerBlock />
+                </Box>
+              </Tab.Panel>
+            )}
+          </Box>
         </Box>
-      </Box>
+      )}
     </Flex>
   );
 };
