bd671bab78efa29b48cb84e43dc7c120315e5b3e
Muneef Hameed <hey@muneef.in>
2025-06-11 21:05:32 +0530

    chores: remve menu link

--- apps/web/src/components/Sidebar/Menulist.tsx
+++ apps/web/src/components/Sidebar/Menulist.tsx
@@ -33,11 +33,11 @@ export const Menulist = [
         icon: <ThumbsUp size={defaultIcon} weight={defaultWeight} />,
         path: '/approvals',
       },
-      {
-        name: 'Repository',
-        icon: <Folder size={defaultIcon} weight={defaultWeight} />,
-        path: '/repository',
-      },
+      // {
+      //   name: 'Repository',
+      //   icon: <Folder size={defaultIcon} weight={defaultWeight} />,
+      //   path: '/repository',
+      // },
     ],
   },
   {
