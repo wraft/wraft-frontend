6ae7f3d47366822908574b8ff17014e973cea666
Salsabeel <k.salsabeel@gmail.com>
2025-07-21 17:16:20 +0530

    fix: initial document load

--- apps/web/src/components/DocumentView/AwarenessUsers.tsx
+++ apps/web/src/components/DocumentView/AwarenessUsers.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { Box, Flex } from '@wraft/ui';
 
 import { useDocument } from './DocumentContext';
@@ -23,24 +24,50 @@ const AwarenessUsers = () => {
         editorRef.current?.provider.awareness.getStates().values(),
       );
 
-      setAwarenessUsers(newStates);
+      const uniqueUsers = newStates.filter(
+        (state: any, index: number, self: any[]) =>
+          index ===
+          self.findIndex(
+            (s: any) =>
+              s.user?.id === state.user?.id ||
+              s.user?.name === state.user?.name,
+          ),
+      );
+
+      setAwarenessUsers(uniqueUsers);
     }
   };
+
+  console.log(
+    'awareness',
+    awarenessUsers.map((item: any) => item.user),
+  );
 
   return (
     <Flex px="xs">
       {awarenessUsers.map((item: any, index: any) => (
-        <Box
-          key={index}
-          w="18px"
-          h="18px"
-          marginRight="-sm"
-          borderColor="border"
-          bg={item?.user?.color}
-          borderRadius="full"
-          data-name={item?.user?.name}>
-          {/* Uncomment to display user name: {item?.user?.color} */}
-        </Box>
+        <>
+          <Box
+            key={index}
+            w="22px"
+            h="22px"
+            marginRight="-sm"
+            border="2px solid"
+            borderColor={item?.user?.color || 'border'}
+            overflow="hidden"
+            bg={item?.user?.color}
+            borderRadius="full"
+            data-name={item?.user?.name}>
+            {item?.user?.image && (
+              <Image
+                src={item?.user?.image}
+                width={20}
+                height={20}
+                alt={item?.user?.name}
+              />
+            )}
+          </Box>
+        </>
       ))}
     </Flex>
   );
